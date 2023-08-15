1c0c43d29160acf908cc39a94e6d2d5ac00f288d
Daniel Bolin <axbolin@iu.edu>
2023-08-15 17:51:36 -0400

    Top level config

--- constants.sh
+++ constants.sh
@@ -9,6 +9,20 @@ export GPG_TTY=$(tty)
 # Load environment
 source env.sh
 
+# Check required configuration
+_=${DATASET:?"No dataset selected!"}
+_=${VERSION:?"No version selected!"}
+
 # Shorthands and configuration options
 export DATASETS_DIR=${DATASETS_DIR:="./datasets"}
 export RAW_DATA_DIR=${RAW_DATA_DIR:="./raw-data"}
+export OUTPUT_DIR="$RAW_DATA_DIR/$DATASET/$VERSION"
+export CACHE_DIR="$OUTPUT_DIR/tmp"
+
+# Load dataset configuration
+source $DATASETS_DIR/$DATASET/config.sh
+
+# Check required dataset configuration
+_=${DATASET_LIST_URL:?"No dataset list url set!"}
+_=${DATASET_ID_COLUMN:?"No dataset list identifier column set!"}
+_=${DATASET_LINK_COLUMN:?"No dataset list link column set!"}
---
+++ env.example.sh
@@ -0,0 +1,6 @@
+# Select dataset
+export DATASET=${DATASET:="sample"}
+export VERSION=${VERSION:="2023-08-15"}
+
+# Tokens
+export HUBMAP_TOKEN="your token"
