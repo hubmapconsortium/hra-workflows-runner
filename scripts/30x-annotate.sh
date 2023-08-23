f5d5a89719b702d43ed337e6785cc368e84d69ce
Daniel Bolin <axbolin@iu.edu>
2023-08-23 14:04:55 -0400

    Fix some env variables

--- scripts/30x-annotate.sh
+++ scripts/30x-annotate.sh
@@ -3,7 +3,7 @@ source constants.sh
 shopt -s extglob
 set -ev
 
-for dir in DATA_REPO_DIR/*/; do
+for dir in $DATA_REPO_DIR/*/; do
   pushd $dir
   sbatch $SRC_DIR/src/slurm/slurm-annotate.sh
   popd
