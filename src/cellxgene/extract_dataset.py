2786a60d89af7b0ea37bacfa59531f1933e6242c
Daniel Bolin <axbolin@iu.edu>
2023-09-26 15:24:10 -0400

    Update cellxgene extract python script

--- src/cellxgene/extract_dataset.py
+++ src/cellxgene/extract_dataset.py
@@ -1,17 +1,53 @@
 import argparse
+import os
+import re
+import typing as t
 from pathlib import Path
+
 import anndata
 
-SAMPLE_ID_COLUMN = 'sample'
-DONOR_ID_COLUMN = 'donor_id'
-TISSUE_COLUMN = 'tissue'
+
+def _get_env_list(key: str, default: t.List[str]) -> t.List[str]:
+    raw = os.environ.get(key)
+    return re.split("[\s,;]+", raw) if raw else default
+
+
+SAMPLE_COLUMNS = _get_env_list("CELLXGENE_COLUMNS_SAMPLE", ["sample", "sample_uuid"])
+DONOR_COLUMNS = _get_env_list("CELLXGENE_COLUMNS_DONOR", ["donor_id"])
+TISSUE_COLUMNS = _get_env_list("CELLXGENE_COLUMNS_TISSUE", ["tissue"])
+
+
+def _find_column(matrix: anndata.AnnData, columns: t.List[str]):
+    for col in columns:
+        if col in matrix.obs.columns:
+            return col
+    return None
+
+
+def _match_column(matrix: anndata.AnnData, columns: t.List[str], value: str):
+    column = _find_column(matrix, columns)
+    return matrix.obs[column] == value if column else None
+
+
+def create_mask(matrix: anndata.AnnData, donor: str, tissue: str, sample: str):
+    if sample:
+        sample_mask = _match_column(matrix, SAMPLE_COLUMNS, sample)
+        if sample_mask is not None:
+            return sample_mask
+
+    donor_mask = _match_column(matrix, DONOR_COLUMNS, donor)
+    tissue_mask = _match_column(matrix, TISSUE_COLUMNS, tissue)
+    if donor_mask is not None and tissue_mask is not None:
+        return donor_mask & tissue_mask
+    return None
+
 
 def main(args: argparse.Namespace):
     data = anndata.read_h5ad(args.file)
-    if args.sample:
-        mask = data.obs[SAMPLE_ID_COLUMN] == args.sample
-    if args.donor and args.tissue:
-        mask = (data.obs[TISSUE_COLUMN] == args.tissue) & (data.obs[DONOR_ID_COLUMN] == args.donor) 
+    mask = create_mask(data, args.donor, args.tissue, args.sample)
+    if mask is None:
+        raise ValueError("Could not filter data")
+
     data[mask].write_h5ad(args.output)
 
 
@@ -23,7 +59,7 @@ def _get_arg_parser() -> argparse.ArgumentParser:
     parser.add_argument("--donor", help="Donor ID")
     parser.add_argument("--tissue", help="Tissue")
     parser.add_argument("--sample", help="Sample ID")
-    parser.add_argument('--output', type=Path, help="Output file")
+    parser.add_argument("--output", type=Path, help="Output file")
 
     return parser
 
