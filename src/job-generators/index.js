252b37d4c493189f3717f2b24be60ca71f4b682b
Daniel Bolin <axbolin@iu.edu>
2023-09-28 15:05:34 -0400

    Remove old code

--- src/job-generators/index.js
+++
@@ -1,8 +0,0 @@
-import * as gtex from './gtex.js';
-import * as hubmap from './hubmap.js';
-
-const GENERATORS = [gtex, hubmap];
-
-export function findGenerator(dir) {
-  return GENERATORS.find((gen) => gen.supports(dir));
-}
