cd1742c11fa21a4c6fc6e39799a14e8adb064864
Daniel Bolin <axbolin@iu.edu>
2023-10-03 14:55:13 -0400

    Improve annotate script

--- constants.sh
+++ constants.sh
@@ -16,8 +16,9 @@ _=${DATASET:?"No dataset selected!"}
 _=${VERSION:?"No version selected!"}
 
 # Shorthands and configuration options
-export DATASETS_DIR=${DATASETS_DIR:="./datasets"}
-export RAW_DATA_DIR=${RAW_DATA_DIR:="./raw-data"}
+export RUNNER=${RUNNER:-"docker"}
+export DATASETS_DIR=${DATASETS_DIR:-"./datasets"}
+export RAW_DATA_DIR=${RAW_DATA_DIR:-"./raw-data"}
 export OUTPUT_DIR="$RAW_DATA_DIR/$DATASET/$VERSION"
 export DATA_REPO_DIR="$RAW_DATA_DIR/data-repo"
 export CACHE_DIR="$RAW_DATA_DIR/tmp"
@@ -25,6 +26,7 @@ export SRC_DIR="./src"
 
 # Absolute path to project. Used when starting slurm jobs
 export PROJECT_DIR="/N/project/hra/hra-workflows-runner"
+export SIF_CACHE_DIR="/N/project/hra/sif-cache"
 
 # Load dataset configuration
 source $DATASETS_DIR/$DATASET/config.sh
--- scripts/30-annotate.sh
+++ scripts/30-annotate.sh
@@ -3,8 +3,40 @@ source constants.sh
 shopt -s extglob
 set -ev
 
-for dir in $DATA_REPO_DIR/*/; do
-  pushd $dir
-  cwl-runner --singularity --tmpdir-prefix $TEMP https://raw.githubusercontent.com/hubmapconsortium/hra-workflows/main/pipeline.cwl job.json
+CWL_PIPELINE="https://raw.githubusercontent.com/hubmapconsortium/hra-workflows/main/pipeline.cwl"
+CWL_OPTS=()
+
+if [[ $RUNNER == "slurm" || $RUNNER == "singularity" ]]; then
+  CWL_OPTS+=(--singularity)
+
+  if [[ -d $SIF_CACHE_DIR ]]; then
+    LINK_SIF_CACHE=true
+  fi
+fi
+
+if [[ -n $TEMP ]]; then
+  CWL_OPTS+=(--tmpdir-prefix $TEMP)
+fi
+
+link_sif_cache() {
+  for SIF in $SIF_CACHE_DIR/*.sif; do
+    FILE=$(basename $SIF)
+    ln -sf $SIF "$1/$FILE"
+  done
+}
+
+for DIR in $(node $SRC_DIR/list-downloaded-dirs.js); do
+  pushd $DIR
+
+  if [[ -n $LINK_SIF_CACHE ]]; then
+    link_sif_cache $DIR
+  fi
+
+  if [[ $RUNNER == "slurm" ]]; then
+    sbatch $PROJECT_DIR/src/slurm/slurm-annotate.sh
+  else
+    cwl-runner ${CWL_OPTS[@]} $CWL_PIPELINE job.json
+  fi
+
   popd
 done
