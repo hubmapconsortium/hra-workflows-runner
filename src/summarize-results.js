f99d70197900587d975f3586d703b4407d2a4984
Daniel Bolin <axbolin@iu.edu>
2023-10-03 14:46:46 -0400

    Summarize results tweak

--- src/summarize-results.js
+++ src/summarize-results.js
@@ -14,6 +14,7 @@ import {
 
 async function updateAlgorithmStatus(item, config) {
   const directory = getDirForId(item.id);
+  item.errors = '';
   for (const algorithm of ALGORITHMS) {
     await readReport(item, algorithm, directory, config);
   }
