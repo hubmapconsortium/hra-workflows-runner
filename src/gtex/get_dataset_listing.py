e22efcc357480930c35e52cc48352b955caad381
Vicky Daiya <vickydaiya10.vd@gmail.com>
2023-11-15 15:33:05 -0500

    gtex done

---
+++ src/gtex/get_dataset_listing.py
@@ -0,0 +1,24 @@
+import argparse
+import os
+import anndata
+from pathlib import Path
+import json
+
+ID_COLUMN = os.environ.get("GTEX_COLUMN_ID", "Sample ID")
+
+
+def main(args: argparse.Namespace):
+    data = anndata.read_h5ad(args.file)
+    print(json.dumps(data.obs[ID_COLUMN].drop_duplicates().tolist()))
+
+
+def _get_arg_parser() -> argparse.ArgumentParser:
+    parser = argparse.ArgumentParser(description="Get the dataset listing for GTEx")
+    parser.add_argument("file", type=Path, help="Main data h5ad file")
+    return parser
+
+
+if __name__ == "__main__":
+    parser = _get_arg_parser()
+    args = parser.parse_args()
+    main(args)
