8dee26269287133bf806e027eb7d65c42378937f
Bruce W. Herr II <bherr@indiana.edu>
2024-02-20 15:14:57 -0500

    Update organ lookup method to use the local crosswalk organs for mapping from tissue id to organ

--- src/cellxgene/organ-lookup.js
+++ src/cellxgene/organ-lookup.js
@@ -1,18 +1,12 @@
+import { readFileSync } from 'fs';
 import Papa from 'papaparse';
 import { checkFetchResponse } from '../util/fs.js';
 import { logEvent } from '../util/logging.js';
 
-/** Endpoint to get available organs */
-const REF_ORGANS_ENDPOINT = 'https://grlc.io/api-git/hubmapconsortium/ccf-grlc/subdir/ccf//ref-organ-terms';
+/** crosswalk files parsed as jsonld */
+const CROSSWALK_DATA = './crosswalking-tables/crosswalks.jsonld';
 /** Endpoint to do tissue to organ query */
 const SPARQL_ENDPOINT = 'https://ubergraph.apps.renci.org/sparql';
-
-/** Extra organs not returned by getOrgans() */
-const EXTRA_ORGANS = {
-  blood: 'UBERON:0000178',
-  bone_marrow: 'UBERON:0002371',
-  adipose_tissue: 'UBERON:0001013',
-};
 
 /**
  * Gets available organs
@@ -20,20 +14,14 @@ const EXTRA_ORGANS = {
  * @returns {Promise<string[]>}
  */
 async function getOrgans() {
-  const resp = await fetch(REF_ORGANS_ENDPOINT, {
-    headers: {
-      Accept: 'application/json',
-    },
-  });
-  checkFetchResponse(resp, 'CellxGene organ lookup failed');
-
-  const items = await resp.json();
-  return items
-    .map((item) => item.representation_of)
-    .filter((id) => id !== undefined)
-    .map((id) => id.slice(id.lastIndexOf('/') + 1))
-    .map((id) => id.replace('_', ':'))
-    .filter((id) => id.startsWith('UBERON'));
+  const organs = new Set();
+  const crosswalks = JSON.parse(readFileSync(CROSSWALK_DATA))['@graph'];
+  for (const { organ_id: organ } of crosswalks) {
+    if (organ?.trim().startsWith('UBERON')) {
+      organs.add(organ);
+    }
+  }
+  return Array.from(organs);
 }
 
 /**
@@ -48,33 +36,28 @@ async function getLookup(organs, ids) {
   const query = `
     PREFIX part_of: <http://purl.obolibrary.org/obo/BFO_0000050>
     PREFIX UBERON: <http://purl.obolibrary.org/obo/UBERON_>
+    PREFIX CL: <http://purl.obolibrary.org/obo/CL_>
 
     SELECT DISTINCT ?as ?organ
     FROM <http://reasoner.renci.org/redundant>
     WHERE {
       VALUES (?organ_id) { ${serializeIds(organs)} }
       VALUES (?as_id) { ${serializeIds(ids)} }
-      {
-        ?as_id part_of: ?organ_id .
-      }
-      UNION
-      {
-        ?organ_id part_of: ?as_id .
-      }
 
-      BIND(REPLACE(STR(?as_id), 'http://purl.obolibrary.org/obo/UBERON_', 'UBERON:') as ?as)
-      BIND(REPLACE(STR(?organ_id), 'http://purl.obolibrary.org/obo/UBERON_', 'UBERON:') as ?organ)
+      ?as_id part_of: ?organ_id .
+
+      BIND(REPLACE(REPLACE(STR(?as_id), STR(UBERON:), 'UBERON:'), STR(CL:), 'CL:') as ?as)
+      BIND(REPLACE(STR(?organ_id), STR(UBERON:), 'UBERON:') as ?organ)
     }
   `;
-  const body = new URLSearchParams({ query });
+  console.log(query);
   const resp = await fetch(SPARQL_ENDPOINT, {
     method: 'POST',
     headers: {
       Accept: 'text/csv',
-      'Content-Type': 'application/x-www-form-urlencoded',
-      'Content-Length': body.toString().length.toString(),
+      'Content-Type': 'application/sparql-query',
     },
-    body,
+    body: query,
   });
   checkFetchResponse(resp, 'CellxGene organ lookup failed');
 
@@ -96,8 +79,7 @@ export async function getOrganLookup(ids) {
   const organs = await logEvent('CellXGene:GetOrgans', () => getOrgans());
   const tissueIds = ids.filter((id) => !organs.includes(id));
   const organToOrganPairs = organs.map((organ) => [organ, organ]);
-  const extraOrganPairs = Object.values(EXTRA_ORGANS).map((organ) => [organ, organ]);
   const tissueToOrganPairs = await logEvent('CellXGene:GetOrganLookup', () => getLookup(organs, tissueIds));
 
-  return new Map([...organToOrganPairs, ...extraOrganPairs, ...tissueToOrganPairs]);
+  return new Map([...organToOrganPairs, ...tissueToOrganPairs]);
 }
