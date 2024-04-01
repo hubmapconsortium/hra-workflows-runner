15fefba2765ea5888f7c1fdc7d8a6276b97a556d
Daniel Bolin <axbolin@iu.edu>
2024-04-01 15:27:11 -0400

    Improve cancel script

--- scripts/30x-cancel.sh
+++ scripts/30x-cancel.sh
@@ -13,6 +13,8 @@ if [[ ! -e "$control_file" ]]; then
   exit 1
 fi
 
+bash "$SRC_DIR/slurm/util/update-active-workers.sh" "$control_file" "-1000000" >/dev/null
+
 (
   flock 9 || exit 1
   printf 'Approximate number of executed jobs: %s\n' "$(<$control_file)"
